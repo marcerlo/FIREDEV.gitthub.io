9486189f2bbfb2420a33934d44cecd88d9bdc63d
marcerlo <nicolaspintadotorre@gmail.com>
2026-02-20 17:16:57 -0500

    20/02/2026/aviffondo

--- script.js
+++ script.js
@@ -50,26 +50,15 @@ var typed = new Typed(".typing-2", {
 
 //carrrusel
 
-$('.carrusel').owlCarousel({
-  margin: 20,
-  loop: true,
-  autoplay: true,
-  autoplayTimeOut: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-      0:{
-          items: 1,
-          nav: false
-      },
-      600:{
-          items: 2,
-          nav: false
-      },
-      1000:{
-          items: 3,
-          nav: false
-      }
-  }
+// carrusel (separado)
+$(window).on("load", function(){
+  $('.carrusel').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+  });
 });
 });
 
